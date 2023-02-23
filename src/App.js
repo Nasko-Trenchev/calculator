330bca5b8c60f81f7876a70ce2b2070f4f250b2f
Nasko-Trenchev <naskotrenchev1@abv.bg>
2023-02-23 11:27:27 +0200

    React initialize and adding HTML

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
+import Screen from './Screen';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div class="container calculator">
+    <Screen/>
+        <dl class="touche__box">
+        <dt class="clear-item"> <span>clear</span></dt>
+        <dt class="touche__box-item"> <span class="soustraction">_</span></dt>
+        <dt class="touche__box-item"><span class="sign">+</span></dt>
+        <dt class="touche__box-item clearme"> <span>7</span></dt>
+        <dt class="touche__box-item"> <span>8</span></dt>
+        <dt class="touche__box-item"> <span>9</span></dt>
+        <dt class="touche__box-item"><span class="sign">/</span></dt>
+        <dt class="touche__box-item"> <span>4</span></dt>
+        <dt class="touche__box-item"> <span>5</span></dt>
+        <dt class="touche__box-item"> <span>6</span></dt>
+        <dt class="touche__box-item"><span class="sign">x</span></dt>
+        <dt class="touche__box-item"> <span>1</span></dt>
+        <dt class="touche__box-item"> <span>2</span></dt>
+        <dt class="touche__box-item"> <span>3</span></dt>
+        <dt class="equal-item"><span class="sign-equal">=</span></dt>
+        <dt class="touche__box-item zero-item"> <span>0</span></dt>
+        <dt class="touche__box-item un-item"><span class="sign">.</span></dt>
+    </dl>
+</div> 
   );
 }
 
---
+++ src/Screen.js
@@ -0,0 +1,6 @@
+export default function Screen(){
+
+    return(
+        <div class="screen-item screen"> <span>screen</span></div>
+    )
+}
